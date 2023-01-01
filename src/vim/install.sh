2d4120937f3e06c2e3bd7ec7743a1f0e89fbd3c4
Masanori Tani <metro@uta8a.org>
2023-01-01 23:52:26 +0000

    fix: pass test of vim features

--- src/vim/install.sh
+++ src/vim/install.sh
@@ -5,16 +5,18 @@ echo "Activating feature 'vim'"
 
 # only support debian, ubuntu
 apt-get update
-apt-get install -y wget curl jq
-
-# install GitHub CLI
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& apt update \
-&& apt install gh -y
+apt-get install -y wget curl jq libfuse2 libglib2.0-0 autoconf automake cproto gettext libacl1-dev libgpm-dev libgtk-3-dev libtinfo-dev libxmu-dev libxpm-dev libncurses-dev libperl-dev liblua5.2-0 liblua5.2-dev libluajit-5.1-dev lua5.2 luajit python-dev python3-dev ruby-dev
 
 # install vim latest AppImage
-wget -O /usr/local/bin/vim $(gh api -H "Accept: application/vnd.github+json" /repos/vim/vim-appimage/releases/latest | jq '. | .assets[] | select(.name | test("AppImage$")) | .browser_download_url' | tr -d '"')
+wget -O /usr/local/bin/vim.AppImage $(curl -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/vim/vim-appimage/releases/latest | jq '. | .assets[] | select(.name | test("AppImage$")) | .browser_download_url' | tr -d '"')
+
+# extract AppImage
+chmod +x /usr/local/bin/vim.AppImage
+
+cat > /usr/local/bin/vim \
+<< \EOF
+#!/bin/sh
+/usr/local/bin/vim.AppImage --appimage-extract-and-run $@
+EOF
 
 chmod +x /usr/local/bin/vim
