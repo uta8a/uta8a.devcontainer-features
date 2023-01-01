ce8b8fd9a5a043368a13ef7357115790986497c4
Masanori Tani <metro@uta8a.org>
2023-01-01 22:16:22 +0000

    feat: add vim feature

--- src/vim/install.sh
+++ src/vim/install.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 set -e
 
-echo "Activating feature 'color'"
-echo "The provided favorite color is: ${FAVORITE}"
+echo "Activating feature 'vim'"
 
-cat > /usr/local/bin/color \
-<< EOF
-#!/bin/sh
-echo "my favorite color is ${FAVORITE}"
-EOF
+# only support debian, ubuntu
+apt-get update
+apt-get install -y wget curl jq
 
-chmod +x /usr/local/bin/color
+# install GitHub CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& apt update \
+&& apt install gh -y
+
+# install vim latest AppImage
+wget -O /usr/local/bin/vim $(gh api -H "Accept: application/vnd.github+json" /repos/vim/vim-appimage/releases/latest | jq '. | .assets[] | select(.name | test("AppImage$")) | .browser_download_url' | tr -d '"')
+
+chmod +x /usr/local/bin/vim
