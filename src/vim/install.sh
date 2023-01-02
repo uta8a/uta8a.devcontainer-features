4eb5d98a683abd0550a1dc6650a4091e6e3914aa
Masanori Tani <metro@uta8a.org>
2023-01-02 00:03:27 +0000

    ci: package python-dev is deprecated

--- src/vim/install.sh
+++ src/vim/install.sh
@@ -5,7 +5,7 @@ echo "Activating feature 'vim'"
 
 # only support debian, ubuntu
 apt-get update
-apt-get install -y wget curl jq libfuse2 libglib2.0-0 autoconf automake cproto gettext libacl1-dev libgpm-dev libgtk-3-dev libtinfo-dev libxmu-dev libxpm-dev libncurses-dev libperl-dev liblua5.2-0 liblua5.2-dev libluajit-5.1-dev lua5.2 luajit python-dev python3-dev ruby-dev
+apt-get install -y wget curl jq libfuse2 libglib2.0-0 autoconf automake cproto gettext libacl1-dev libgpm-dev libgtk-3-dev libtinfo-dev libxmu-dev libxpm-dev libncurses-dev libperl-dev liblua5.2-0 liblua5.2-dev libluajit-5.1-dev lua5.2 luajit python3-dev ruby-dev
 
 # install vim latest AppImage
 wget -O /usr/local/bin/vim.AppImage $(curl -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/vim/vim-appimage/releases/latest | jq '. | .assets[] | select(.name | test("AppImage$")) | .browser_download_url' | tr -d '"')
